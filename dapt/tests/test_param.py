11ee9d989f23d049498e3c69fd550d9b1b4d5e62
BenSDuggan <dugganbens@gmail.com>
2019-10-26 16:25:22 -0400

    adding param tests and lib updates

--- dapt/tests/test_param.py
+++ dapt/tests/test_param.py
@@ -5,7 +5,7 @@
 """
 
 import dapt
-import os, csv
+import os, csv, datetime
 from collections import OrderedDict 
 
 # Create a test file with just the required fields
@@ -20,11 +20,12 @@ def create_simple_test_file():
 # Create a test file that has all the possible fields
 def create_complex_test_file():
 	with open('test.csv', 'w') as f:
-		writer = csv.DictWriter(f, fieldnames=['id', 'startTime', 'endTime', 'status', 'comment','performed-by', 'a', 'b', 'c'])
+		writer = csv.DictWriter(f, fieldnames=['id', 'start-time', 'end-time', 'status', 'comment','performed-by', 'a', 'b', 'c'])
 		writer.writeheader()
-		writer.writerow({'id':'t1', 'startTime':'2019-09-06 17:23', 'endTime':'2019-09-06 17:36', 'status':'finished', 'comment':'2+4=6','performed-by':'ben', 'a':'2', 'b':'4', 'c':'6'})
-		writer.writerow({'id':'t2', 'startTime':'', 'endTime':'', 'status':'', 'comment':'','performed-by':'', 'a':'10', 'b':'10', 'c':''})
-		writer.writerow({'id':'t3', 'startTime':'', 'endTime':'', 'status':'', 'comment':'','performed-by':'', 'a':'10', 'b':'-10', 'c':''})
+		writer.writerow({'id':'t1', 'start-time':'2019-09-06 17:23', 'end-time':'2019-09-06 17:36', 'status':'finished', 'comment':'2+4=6','performed-by':'ben', 'a':'2', 'b':'4', 'c':'6'})
+		writer.writerow({'id':'t2', 'start-time':'', 'end-time':'', 'status':'', 'comment':'','performed-by':'', 'a':'10', 'b':'10', 'c':''})
+		writer.writerow({'id':'t3', 'start-time':'', 'end-time':'', 'status':'', 'comment':'','performed-by':'', 'a':'10', 'b':'-10', 'c':''})
+
 
 # Test that the next paramater set can be retrived
 def test_Param_next_parameters():
@@ -39,8 +40,6 @@ def test_Param_next_parameters():
     
 	assert actual == expected, "Cannot get the next paramater set."
 
-
-
 # Test if the status can be updated
 def test_Param_update_status():
     create_simple_test_file()
@@ -54,9 +53,6 @@ def test_Param_update_status():
     expected = OrderedDict({'id':'t2', 'status':'adding', 'a':'10', 'b':'10', 'c':''})
 
     assert actual == expected, "Cannot update the status of the paramater set."
-
-
-
 
 # Test if failed works
 def test_Param_successful():
@@ -83,7 +79,13 @@ def test_Param_successful_fields():
     expected = actual
     actual = param.successful(actual['id'])
     
-    expected = OrderedDict({'id':'t2', 'startTime':'', 'endTime':'', 'status':'failed', 'comment':'','performed-by':'', 'a':'10', 'b':'10', 'c':''})
+    expected = OrderedDict({'id':'t2', 'start-time':'', 'end-time':'', 'status':'finished', 'comment':'','performed-by':'', 'a':'10', 'b':'10', 'c':''})
+
+    # Check to see if the time in the database are within 15 seconds of ours.
+    if (datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S') - datetime.datetime.strptime(actual['start-time'], '%Y-%m-%d %H:%M:%S')).seconds < 15:
+        expected['start-time'] = actual['start-time']
+    if (datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S') - datetime.datetime.strptime(actual['end-time'], '%Y-%m-%d %H:%M:%S')).seconds < 15:
+        expected['end-time'] = actual['end-time']
     
     assert actual == expected, "Cannot update the status of the paramater set."
 
@@ -112,6 +114,12 @@ def test_Param_failed_fields():
     expected = actual
     actual = param.failed(actual['id'], 'This is an error')
 
-    expected = OrderedDict({'id':'t2', 'startTime':'', 'endTime':'', 'status':'failed', 'comment':'','performed-by':'', 'a':'10', 'b':'10', 'c':''})
+    expected = OrderedDict({'id':'t2', 'start-time':'', 'end-time':'', 'status':'failed', 'comment':'','performed-by':'', 'a':'10', 'b':'10', 'c':''})
+
+    # Check to see if the time in the database are within 15 seconds of ours.
+    if (datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S') - datetime.datetime.strptime(actual['start-time'], '%Y-%m-%d %H:%M:%S')).seconds < 15:
+        expected['start-time'] = actual['start-time']
+    if (datetime.datetime.strptime(datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S') - datetime.datetime.strptime(actual['end-time'], '%Y-%m-%d %H:%M:%S')).seconds < 15:
+        expected['end-time'] = actual['end-time']
     
     assert actual == expected, "Cannot update the status of the paramater set."
