5208bc60d304ed95a569d07d77ef968561bd6299
BenSDuggan <dugganbens@gmail.com>
2019-10-26 18:09:13 -0400

    updating travis-ci and improving commandline

--- dapt/__init__.py
+++ dapt/__init__.py
@@ -8,30 +8,27 @@ This is __init__.py
 
 __version__ = "0.8.2"
 
-import sys
+import sys, argparse
 from . import config
 
-# Parse command line arguments
-if len(sys.argv) > 1:
-    # Config settings
-    if sys.argv[1] == 'config':
-        file_name = 'c.txt'
+parser = argparse.ArgumentParser(description='Distributed Automated Parameter Testing (DAPT)\nA library to assist with running parameter sets across multiple systems.', formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument('--f', metavar='config.json', default='config.json', type=str, action='store', help="The path to the config file.")
+parser.add_argument('--l', action='store_true', help="Reset the value of \'last-test\' to None.")
+parser.add_argument('--c', action='store_true', help="Create a blank config file.")
+parser.add_argument('--s', action='store_true', help="Remove keys from the config file so it can be made public.")
 
-        # Check for flags
-        try:
-            if '-f' in sys.argv[1:]:
-                file_name = sys.argv[sys.argv.index('-f')+1]
-        except:
-            print('Couldn\'t use specified file.  Using file name "config.txt".')
-
-        # Reset config file
-        if sys.argv[2] == 'create':
-            config.Config.create(file_name)
-
-        # Safe config file
-        if sys.argv[2] == 'safe':
-            config.Config.safe(file_name)
-    #exit()
+args = parser.parse_args()
+if args.l:
+    # Remove last-test from config file
+    conf = config.Config(args.f)
+    conf.config['last-test'] = None
+    conf.update_config()
+if args.c:
+    # Reset config file
+    config.Config.create(args.f)
+if args.s:
+    # Safe config file
+    config.Config.safe(args.f)
 
 from . import box
 from . import database
