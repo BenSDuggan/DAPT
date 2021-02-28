6785eefebb6d96ef05050562f94c1eb7b20a9ad5
BenSDuggan <dugganbens@gmail.com>
2021-02-28 13:34:46 -0500

    removing logging from __init__

--- dapt/__init__.py
+++ dapt/__init__.py
@@ -10,17 +10,3 @@ from .db import *
 from .storage import *
 from .param import Param
 from .tools import *
-
-# Temperatry loggin
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-
-# add formatter to ch
-ch.setFormatter(logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
-
-# add ch to logger
-logger.addHandler(ch)
